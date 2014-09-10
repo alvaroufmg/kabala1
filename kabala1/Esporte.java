1f6c7e973bbac74a36cb48cdeb5918dba2c47e28
eliassoares <eliassoares@dcc.ufmg.br>
2014-09-09 23:46:28 -0300

    Mandando

---
+++ kabala1/Esporte.java
@@ -0,0 +1,7 @@
+//importar bibliotecas
+
+public abstract class Esporte {
+
+	//Em cada esporte será implementada a partida
+	public void partida();
+}
---
+++ kabala1/Selecao.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import Atleta; 
+
+public class Selecao {
+	
+	ArrayList<Atleta> atletas = new ArrayList<Atleta>();
+	
+	
+	
+}
