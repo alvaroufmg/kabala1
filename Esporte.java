5e3a3acc74c35a2d7a1f3cce8647bf2a3fa48e3e
eliassoares <eliassoares@dcc.ufmg.br>
2014-09-11 11:36:44 -0300

    Adicionando uma Classe de teste TP1PM.java

--- Esporte.java
+++ Esporte.java
@@ -1,18 +1,20 @@
-//importar bibliotecas
+
+import java.util.ArrayList;
 
 public abstract class Esporte {
 
-	private ArrayLists
+	private ArrayLists <Selecao> selecoes = new ArrayList<Selecao>();
 	
 	//Em cada esporte será implementada a partida
 	public void partida();
 	
-	//Composição, a classe Esporte "TEM" uma classe seleção.
-	public class Selecao {
+	//Composição, a classe Esporte "TEM" uma classe seleção. Apenas classes filhas e do pacote poderam acessar Selecao.
+	protected class Selecao {
 		
 		private String name;
 		private int id;
-
+		private ArrayList <Atletas> atletas = new ArrayList <Atletas>(); 
+		
 		public String getName() {
 			return this.name;
 		}
@@ -28,6 +30,27 @@ public abstract class Esporte {
 		public void setId(int id) {
 			this.id = id;
 		}
-	}
+		
+		//A classe Selecao "TEM" Atletas, portanto, Atletas compôe Selecao.
+		protected class Atletas {
+			private String name;
+			private int id;
+
+			public String getName() {
+				return this.name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public int getId() {
+				return this.id;
+			}
+
+			public void setId(int id) {
+				this.id = id;
+			}
+		}
 	}
 }
